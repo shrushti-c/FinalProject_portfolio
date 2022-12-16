b5e5e360375568d5aef5ca69b4c66a311c298d55
shrushti-c <53366053+shrushti-c@users.noreply.github.com>
2022-12-16 13:18:25 -0800

    Add files via upload

---
+++ src/pages/Projects.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import '../App.css';
+function Projects() {
+  return <>  
+  <div className='projects'>
+      <div className='projects-container'>
+        <div className='card'>
+          <h3>--Project 1--</h3>
+          <span className='bar'></span>
+          <h2>API Project</h2>
+          <p>An API to search Images for anything you want. Here's a link to access it:</p>
+          <a href='https://playful-gnome-4b18a1.netlify.app' target='/blank'>Click Here</a>
+          </div>
+        </div>
+        </div> 
+    <div className='projects'>
+      <div className='projects-container'>
+        <div className='card'>
+          <h3>--Project 2--</h3>
+          <span className='bar'></span>
+          <h2>Andoid Based Application</h2>
+          <p>Android based Application to Provide Platform for Small Scale Industries using Android Studio and Google Firebase for the database. The below link shows the publication for the same.</p>
+          <a href='https://www.ijsrd.com/articles/IJSRDV6I110358.pdf' target='/blank'>Click Here</a>
+          </div>
+        </div>
+        </div>
+  <div className='projects'>
+      <div className='projects-container'>
+        <div className='card'>
+          <h3>--Project 3--</h3>
+          <span className='bar'></span>
+          <h2>To-Do List</h2>
+          <p>Created a To-do list with HTML, JS Function and CSS with options to add, delete or marks the tasks as done from the list. The list also shows the tasks you have planned and finished. Below is the link to webpage:</p>
+          <a href='https://courageous-cocada-9a265e.netlify.app/' target='/blank'>Click Here</a>
+          </div>
+        </div>
+
+    </div>
+    <div className='projects'>
+      <div className='projects-container'>
+        <div className='card'>
+          <h3>--Project 4--</h3>
+          <span className='bar'></span>
+          <h2>Fizz Buzz </h2>
+          <p>Show multiples of 3 as "Fizz". For multiples of 5 as "Buzz".For numbers which are multiples of both 3 and 5 as "FizzBuzz".</p>
+          <a href='https://lucky-hummingbird-41af27.netlify.app'>Click Here</a>
+          
+          </div>
+        </div>
+
+    </div>
+    <div className='projects'>
+      <div className='projects-container'>
+        <div className='card'>
+          <h3>--Project 5--</h3>
+          <span className='bar'></span>
+          <p>A Multipage website to showcase my portfolio and to know a bit more about me.</p>
+          <h2>Portfolio in HTML, CSS </h2>
+  
+          <a href='https://joyful-genie-8800b5.netlify.app/about.html'>Click Here</a>
+          
+          </div>
+        </div>
+
+    </div>
+    <div className='projects'>
+      <div className='projects-container'>
+        <div className='card'>
+          <h3>--Project 6--</h3>
+          <span className='bar'></span>
+          <h2>Tic Tac Toe</h2>
+  <p>A simple user friendly Tic Tac Toe game using Javascript function, HTML, CSS. </p>
+          <a href='https://preeminent-snickerdoodle-19d3f0.netlify.app'>Click Here</a>
+          
+          </div>
+        </div>
+
+    </div>
+    <div className='projects'>
+      <div className='projects-container'>
+        <div className='card'>
+          <h3>--Project 7--</h3>
+          <span className='bar'></span>
+          <h2>Guessing Number Game</h2>
+  
+          <a href='https://extraordinary-rolypoly-d16054.netlify.app'>Click Here</a>
+          
+          </div>
+        </div>
+
+    </div>
+    </>
+
+}
+
+export default Projects
---
+++ src/pages/Skills.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Skills.css'
+function Skills() {
+  return <>
+  
+  <h1>Technical Skills</h1>
+  <div className='parent'>
+<div className='card' >
+
+  <h4>Programming languages:</h4>
+  <h4>C, C++, Java, HTML, SQL</h4>
+</div>
+<div className='card' >
+
+<h4>Software's:</h4>
+<h4>RStudio, Eclipse, BIRT tool, STS</h4>
+</div>
+<div className='card' >
+
+<h4>Databases:</h4>
+<h4>MS Excel, MySQL developer</h4>
+</div>
+<div className='card' >
+
+<h4>Operating Systems:</h4>
+<h4>Linux, Windows</h4>
+</div>
+</div>
+    <div>
+        <h1>Work Experience</h1>
+        <h2>Software Developer - Tata Consultancy Services Ltd, India</h2>
+        <h3>From Jan 2020 - July 2022</h3>
+        <p> The objective of the project is to develop an application which allows
+users to find appropriate insurance policies. Through that application they are also able to find help to get more information about getting Life, Pension, Death Claims, Mutual funds and how to claim those bills.I receive unstructured Raw data and then transform it to reports to renew Insurance Policies through tools like BIRT, STS bundle. Data flows from the source to the main file through pipelines. I write stored procedures for mapping data from its origin to the main table using MySql developer before the data moves to the next team for further processing. Maintained effective communication, both written and oral, with external customers. Hands-on experience of working with engineering staff on all levels, interaction, and meeting with clients. Relationship building abilities to maintain excellent rapport with management and co-workers, customer service.    </p>
+    </div>
+    </>
+}
+
+export default Skills
